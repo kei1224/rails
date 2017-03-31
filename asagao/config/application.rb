49e77796d310b457f3e22938fee34f6044f652d0
Yamashita Satoshi <satoshi.yamashita1224@gmail.com>
2017-03-31 18:10:51 +0900

    第6章まで終了

--- asagao/config/application.rb
+++ asagao/config/application.rb
@@ -12,5 +12,7 @@ module Asagao
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
+    config.action_controller.permit_all_parameters = true
   end
 end
